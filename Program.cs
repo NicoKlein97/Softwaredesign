ff155b2989052c29db54fde50c8b42ff678fee60
Nico Klein <34275116+NicoKlein97@users.noreply.github.com>
2018-10-18 18:19:46 +0200

    Angefangen

--- Program.cs
+++ Program.cs
@@ -7,7 +7,51 @@ namespace Softwaredesign
         static void Main(string[] args)
         {
             Console.WriteLine("dotnet run, dotnet build, ");
-            Console.WriteLine(args[0]);
+            //Console.WriteLine(args[0]);
+            Program.calculations(123);
+            
+
+        }
+
+        static void calculations(int _number){
+            int number = _number;
+            if (number.ToString().Length == 1){
+                Console.WriteLine("Länge 1");
+                Program.calculateUnits(number);
+            }else if(number.ToString().Length == 2){
+                Console.WriteLine("Länge 2");
+            }else if(number.ToString().Length == 3){
+                Console.WriteLine("Länge 3");
+            }
+            
+        }
+
+        static calculateUnits(int _number){
+            String romanNumber;
+            switch(_number){
+                case 1:
+                    romanNumber = "I";
+                    break;
+                case 2:    
+                    romanNumber = "II";
+                    break;
+                case 3:
+                    romanNumber = "III";
+                    break;
+                case 4:
+                    romanNumber = "IV";
+                    break;
+                case 5:
+                    romanNumber = "V";
+                    break;
+                case 6:
+                    romanNumber = "VI";
+                    break;
+                case 7: 
+                    romanNumber = "VII";
+                    break;                
+
+            }
         }
     }
 }
