685cb7655d930ff2925812941a63f0211eddcc3d
Nico Klein <34275116+NicoKlein97@users.noreply.github.com>
2018-10-20 13:58:51 +0200

    Neuer Ordner

---
+++ Aufgabe1/RomanNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aufgabe1
+{
+    class RomanNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
