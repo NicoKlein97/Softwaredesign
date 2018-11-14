c48020ffc5be0e111d6bcc07eb321a4bb1c61968
Nico Klein <34275116+NicoKlein97@users.noreply.github.com>
2018-11-14 09:26:34 +0100

    Aufgabe 5 erstellt

--- Aufgabe5/Program.cs
+++ Aufgabe5/Program.cs
@@ -7,24 +7,9 @@ namespace Aufgabe5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte geben Sie den Satz ein, welchen Sie umgedreht haben wolen");
-            String satz = Console.ReadLine();
 
-            Console.WriteLine(satz + " -------> " + umdrehen(satz));
+            Console.WriteLine("Hello World");
 
-        }
-
-        public static string umdrehen(String _satz)
-        {
-            List<char> characters = new List<char>();
-
-            for (int i = _satz.Length - 1; i > -1; i--)
-            {
-                characters.Add(_satz[i]);
-            }
-            string reversedString = string.Join("", characters.ToArray());
-            return reversedString;
-            //test
         }
     }
 }
